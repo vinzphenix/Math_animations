eeedf2d411f3d4c39d747e8a0db638b5ac217c9a
vincent <vincent.degrooff@uclouvain.be>
2025-11-21 12:29:39 +0100

    transparent lines

--- Tikz/tsfm_pdf_to_png.sh
+++ Tikz/tsfm_pdf_to_png.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Dossier contenant les fichiers PDF
-input_directory="./vincent/pdfs"
+input_directory="./inmema/pdfs"
 
 # Dossier de sortie pour les fichiers PNG
-output_directory="./vincent/pngs"
+output_directory="./inmema/pngs"
 
 # Créer le dossier de sortie s'il n'existe pas
 mkdir -p "$output_directory"
@@ -12,7 +12,8 @@ mkdir -p "$output_directory"
 # Boucle pour convertir chaque PDF en PNG avec fond blanc
 for pdf in "$input_directory"/*.pdf; do
   output_file="$output_directory/$(basename "$pdf" .pdf).png"
-  convert -density 300 "$pdf" -background white -alpha off "$output_file"
+  convert -density 300 "$pdf" "$output_file"
+  #convert -density 300 "$pdf" -background white -alpha off "$output_file"
 done
 
 echo "Conversion terminée !"
